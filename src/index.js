5434f6ad812f65fac54f7bc12258c94362061c65
Dmytro <itsdelka001@gmail.com>
2025-08-16 14:46:26 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import TradeonPage from './tradeon'; // Імпортуємо сторінку Tradeon
+import TradeonPage from './pages/tradeon'; // Імпортуємо сторінку Tradeon
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
